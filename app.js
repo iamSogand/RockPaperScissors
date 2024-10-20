677cccbced46863b0d5c0c553c80f70c2d21c3be
imSogand <imsogand@outlook.com>
2024-10-20 17:01:08 +0200

    replacing arrow function and if- with shortform

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
 let gameIsRunning = false;
 
-const getPlayerChoice = function () {
+const getPlayerChoice = () => {
   const selection = prompt(`${ROCK},${PAPER}, ${SCISSORS}`, '').toUpperCase();
 
   if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
@@ -20,7 +20,7 @@ const getPlayerChoice = function () {
   return selection;
 };
 
-const getComputerChoice = function () {
+const getComputerChoice = () => {
   const randomValue = Math.random();
   if (randomValue < 0.34) {
     return ROCK;
@@ -31,21 +31,29 @@ const getComputerChoice = function () {
   }
 };
 
-const getWinner = function (cChoice, pChoice) {
-  if (cChoice === pChoice) {
-    return RESULT_DRAW;
-  } else if (
-    (cChoice === ROCK && pChoice === PAPER) ||
-    (cChoice === PAPER && pChoice === SCISSORS) ||
-    (cChoice === SCISSORS && pChoice === ROCK)
-  ) {
-    return RESULT_PLAYER_WINS;
-  } else {
-    return RESULT_COMPUTER_WINS;
-  }
+const getWinner = (cChoice, pChoice) => {
+  cChoice === pChoice
+    ? RESULT_DRAW
+    : (cChoice === ROCK && pChoice === PAPER) ||
+      (cChoice === PAPER && pChoice === SCISSORS) ||
+      (cChoice === SCISSORS && pChoice === ROCK)
+    ? RESULT_PLAYER_WINS
+    : RESULT_COMPUTER_WINS;
+
+  //   if (cChoice === pChoice) {
+  //     return RESULT_DRAW;
+  //   } else if (
+  //     (cChoice === ROCK && pChoice === PAPER) ||
+  //     (cChoice === PAPER && pChoice === SCISSORS) ||
+  //     (cChoice === SCISSORS && pChoice === ROCK)
+  //   ) {
+  //     return RESULT_PLAYER_WINS;
+  //   } else {
+  //     return RESULT_COMPUTER_WINS;
+  //   }
 };
 
-startGameBtn.addEventListener('click', function () {
+startGameBtn.addEventListener('click', () => {
   gameIsRunning = true;
   const playerChoice = getPlayerChoice();
   const computerChoice = getComputerChoice();
